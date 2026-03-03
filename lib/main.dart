021043e23a3a91a69d7f23f9e83df43d1aeed84f
eman-25 <emany0405@gmail.com>
2026-03-03 22:39:50 +0300

    Delete screens that are not needed(verification ans cheng password)

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ import 'screens/Authantication Screen/register_screen.dart';
 import 'screens/Authantication Screen/personal_info_screen.dart';
 import 'screens/Authantication Screen/register_success_screen.dart';
 import 'screens/Authantication Screen/forgot_password_screen.dart';
-import 'screens/Authantication Screen/verification_screen.dart';
-import 'screens/Authantication Screen/change_password_screen.dart';
 import 'screens/Authantication Screen/password_changed_screen.dart';
 import 'Screens/HOME/home_screen.dart';
 import 'models/user_data.dart';
@@ -150,8 +148,6 @@ class DoselyApp extends StatelessWidget {
         '/personalInfo': (context) => const PersonalInfoScreen(),
         '/registerSuccess': (context) => const RegisterSuccessScreen(),
         '/forgotPassword': (context) => const ForgotPasswordScreen(),
-        '/verification': (context) => const VerificationScreen(),
-        '/change-password': (context) => const ChangePasswordScreen(),
         '/passwordChanged': (context) => const PasswordChangedScreen(),
         '/home': (context) => const HomeScreen(),
         '/settings': (context) => const SettingsPanel(),
