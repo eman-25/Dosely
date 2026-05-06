b111295cd48bb61958b88b502bdbe15bf614f133
U88loo <Lilliehj323@gmail.com>
2026-05-06 14:56:19 +0300

    to make Notif work

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true 
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.0.4'
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
   // Import the Firebase BoM
   implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
 
